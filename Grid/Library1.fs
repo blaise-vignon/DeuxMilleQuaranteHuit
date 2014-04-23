89b667e0b3b50489a30bd3619528a0bed043867c
BlaiseV <blaisev@live.ie>
2014-04-23 15:23:00 +0200

    Works pretty well now. Still a bug past the 2048 tile (int overflow, I guess)

--- Grid/Library1.fs
+++ Grid/Library1.fs
@@ -2,7 +2,6 @@
 
 
     type direction = Up | Down  | Left  | Right 
-    //type direction = Up | Down  | Right
 
     let printgrid (d:int[,]) =
         for i in [0..3] do
@@ -17,11 +16,9 @@
             | Left -> printfn "Left"
             | Down -> printfn "Down"
 
-    let getcol col (grid:int[,]) = 
-        Array.init 4 (fun i -> grid.[i,col]) |> Array.toList
+    let getcol col (grid:int[,]) = [grid.[0,col];grid.[1,col];grid.[2,col];grid.[3,col]]
 
-    let getrow row (grid:int[,]) = 
-        Array.init 4 (fun i -> grid.[row, i]) |> Array.toList
+    let getrow row (grid:int[,]) = [grid.[row, 0];grid.[row, 1];grid.[row, 2];grid.[row, 3]]
 
     let rec collapse actionworked ls = 
         // first, remove zeroe
@@ -35,8 +32,6 @@
         let nozeroesls = removezeroes ls
         match nozeroesls with 
             | [0;0;0;0] | [0;0;0] | [0;0] | [0] -> nozeroesls
-            | 0 :: x -> actionworked := true
-                        List.concat [collapse actionworked x; [0]]
             | x :: y :: rst -> if (x = y && x>0) then 
                                                 actionworked := true
                                                 List.concat [[2*x];collapse actionworked rst; [0]]
@@ -136,36 +131,16 @@
     let scoregrid grid = 
         let empties =  grid |> findZeroes |> List.length
         let baseScore = grid |> makeList |> List.mapi (fun i x -> i*i*x*x) |> List.sum
-        let mutable score = baseScore
-//        let mutable maxbn = true
-//
-//        for i in [0..3] do
-//            for j in [0..3] do
-//                if ((Array2D.get grid i j) > (Array2D.get grid 3 3)) then maxbn <- false
-//        
-//        if maxbn then score <- score + 100 * (Array2D.get grid 3 3) * (Array2D.get grid 3 3)
-//        
+        let mutable score = baseScore       
         for i in [0..3] do
             let mutable maxrel = true
             for j in [0..2] do
                 if ((Array2D.get grid i j) > (Array2D.get grid i 3)) then maxrel <- false
             if maxrel then score <- score + 100 * (Array2D.get grid 3 3) * (Array2D.get grid 3 3)
-//
-//        for i in [3..1] do
-//            for j in [3] do
-//                match (Array2D.get grid i j) >= (Array2D.get grid (i-1) j)  with
-//                    | true  -> score <- score + 2 * (Array2D.get grid i j)
-//                    | false  -> score <- score - 2 * (Array2D.get grid (i-1) j)
-//            for j in [2] do
-//                match (Array2D.get grid i j) <= (Array2D.get grid (i-1) j)  with
-//                    | true  -> score <- score + 2 * (Array2D.get grid i j)
-//                    | false  -> score <- score - 2 * (Array2D.get grid (i-1) j)
+
         score
 
-//    let test = Array2D.init 4 4 (fun i j -> if (j > 1) then 2048 else 0)
-//    printgrid test
 
-//
 
     let isLost g = 
         let mutable lost =  true
@@ -180,8 +155,6 @@
         List.nth ls (rnd.Next(n-1))
         
     let rndnew = fun _ ->  if rnd.Next(0,9) = 0 then 4 else 2
-    //let rndnew = if rnd.Next(0,9) = 0 then 4 else 2
-        
 
     let newtile grid = 
         let (m, n) = grid |> findZeroes |> pick in
@@ -199,7 +172,10 @@
             Array2D.set g2 m n i
             yield g2]
 
-
+    let avg l = 
+        match l with 
+        | [] -> -100000.0
+        | _ -> List.average l
 
 
     let rec score n grid  =
@@ -212,8 +188,8 @@
                                     yield gout]
             let scoreList n (g:List<int[,]>) = g |> List.map (score n) |> List.max
             
-            let score2 = enumeratenext grid 2 |> List.map genPoss|> List.map (scoreList (n-1) ) |> List.average
-            let score4 = enumeratenext grid 4 |> List.map genPoss|> List.map (scoreList (n-1) ) |> List.average
+            let score2 = enumeratenext grid 2 |> List.map genPoss|> List.map (scoreList (n-1) ) |> avg
+            let score4 = enumeratenext grid 4 |> List.map genPoss|> List.map (scoreList (n-1) ) |> avg
             let scoreRes = (float score2) * 0.9 + (float score2) * 0.1
             scoreRes
 
@@ -228,8 +204,8 @@
                 match grid |> findZeroes |> List.length with
                 | n when n > 10 -> scoreList <- (dir,score 0 grid) :: scoreList
                 | n when n > 6 -> scoreList <- (dir,score 1 grid) :: scoreList
-//                | n when n > 3 -> scoreList <- (dir,score 3 grid) :: scoreList
-                | _ -> scoreList <- (dir,score 2 grid) :: scoreList
+                | n when n > 3 -> scoreList <- (dir,score 2 grid) :: scoreList
+                | _ -> scoreList <- (dir,score 3 grid) :: scoreList
 
             let elimLeft (d,s) = if (d=Left) then (d,s-1000.0) else (d,s)
             let bestdir = scoreList |> List.map elimLeft |> List.maxBy snd |> fst
